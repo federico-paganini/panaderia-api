817c5cb77dba180b7ac6f2a63165f0779360e5e2
federico-paganini <perfedefer@gmail.com>
2023-09-05 17:33:30 -0300

    Inicio de Sesión - Registro
    Formularios de Inicio de sesión y registro terminados, aplicado estilos en bootstrap, falta aplicar estilo y suavizar intercambio de formularios

--- js/login.js
+++ js/login.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+    const blocklog = document.getElementById("block-log");
+    const blockreg = document.getElementById("block-reg");
+    const formlog = document.getElementById("form-log");
+    const formreg = document.getElementById("form-reg");
+
+    blocklog.addEventListener("click", function () {
+            formlog.style.display = "block";
+            formreg.style.display = "none";
+        })
+
+    blockreg.addEventListener("click", function () {
+        formlog.style.display = "none";
+        formreg.style.display = "block";
+    })
 });
